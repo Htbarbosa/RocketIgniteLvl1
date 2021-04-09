d6a6c28340d1cc6be4293549faa851967592711e
Htbarbosa <henrique_telesbarbosa@hotmail.com>
2021-04-09 16:25:03 -0300

    Challenge Completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const booleanToggle = tasks.map(task => task.id === id ? { 
+      ...task,
+       isComplete: !task.isComplete 
+    } : task)
+
+    setTasks(booleanToggle);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const filteredState = tasks.filter( task => task.id != id );
+
+    setTasks(filteredState);
   }
 
   return (
